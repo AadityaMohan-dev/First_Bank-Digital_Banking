e7fb08af43d0de529f314cf04603d69f0beaf90f
aaditya mohan <aadimohan51@gmail.com>
2024-04-29 19:45:19 +0530

    Components Structure added.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
    <> 
-   <div id="hi" className='text-3xl text-purple-800'>Hello project is workng and all the libraries include sucessfully</div>
+   <div id="hi" className='text-3xl flex justify-center text-purple-800'>Lets Get Some Work Done, In the Game , I Play For Keeps.</div>
    </>
   )
 }
---
+++ src/components/screens/MyPriviliges.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MyPriviliges() {
+  return (
+    <div>MyPriviliges</div>
+  )
+}
