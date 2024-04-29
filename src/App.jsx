0c4ff7604f89147354597a5fd2dac3395b86fd4b
aaditya mohan <aadimohan51@gmail.com>
2024-04-29 19:34:38 +0530

    inital project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <> 
+   <div id="hi" className='text-3xl text-purple-800'>Hello project is workng and all the libraries include sucessfully</div>
+   </>
+  )
+}
+
+export default App
